0316f9345586d5d9a98720d13054a76ae13eb7a8
Wallett95 <103010775+Wallett95@users.noreply.github.com>
2022-04-12 20:22:00 -0600

    Update Run.ps1

--- Run.ps1
+++ Run.ps1
@@ -1,3 +1,5 @@
+#Requires -RunAsAdministrator
+
 #Sets UAC to 0
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
 
