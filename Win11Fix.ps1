9a4440bc0a087211ced8b8f201added8fcaa16fd
Wallett95 <103010775+Wallett95@users.noreply.github.com>
2022-04-12 20:53:58 -0600

    Update Win11Fix.ps1

--- Win11Fix.ps1
+++ Win11Fix.ps1
@@ -6,9 +6,6 @@ Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\C
 #Restores right click context menu
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 timeout /t 2
-#Disables rounded window edges
-start-process .\win11sc.exe
-timeout /t 10
 taskkill /f /im explorer.exe
 timeout /t 1
 start-process explorer.exe
